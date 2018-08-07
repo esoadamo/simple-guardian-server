55019b47249744dbd20aa5e0af685938df3102f7
Adam Hlaváček <git@adamhlavacek.com>
2018-08-07 09:39:32 +0200

    add some web gui

--- simple-guardian-server.py
+++ simple-guardian-server.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template, session, request, redirect, url_for
+from flask import Flask, render_template, session, request, redirect, url_for, send_from_directory
 import socketio
 import eventlet.wsgi
 from http_socket_server import HTTPSocketServer
+from flask_sqlalchemy import SQLAlchemy
 
 sio = socketio.Server()
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text, unique=True, nullable=False)
+    email = db.Column(db.Text, unique=True, nullable=False)
+    password = db.Column(db.Text, unique=False, nullable=False)
+
+
+class Device(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Text, unique=True, nullable=False)
+    secret = db.Column(db.Text, unique=False, nullable=True)
+    config = db.Column(db.Text, unique=False, nullable=False, default="{}")
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('devices', lazy=True))
+
+
+class Rule(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    config = db.Column(db.Text, unique=False, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('rules', lazy=True))
 
 
 @app.route("/api/serviceName")
@@ -12,19 +38,19 @@ def get_new_sid():
     return "simple-guardian-server"
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    return send_from_directory('static', 'register.html')
+
+
+@app.route("/")
+def homepage():
+    return send_from_directory('static', 'welcome.html')
+
+
 if __name__ == '__main__':
-    hsoc = HTTPSocketServer(app)
-
-    def connect(soc):
-        print(soc.sid, 'just connected')
-        import time
-        time.sleep(2)
-        soc.emit('hello', 'how are you?')
-
-    def greetings(soc, data):
-        print(soc.sid, ':', data)
-
-    hsoc.on('connect', connect)
-    hsoc.on('helloBack', greetings)
+    db.create_all()
+    h_soc = HTTPSocketServer(app)
     app = socketio.Middleware(sio, app)
     eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
